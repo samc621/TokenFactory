991323511bea0c3b578c4da81f2e24077228491c
samuelc621 <sam.corso@newtonx.com>
2019-05-30 10:40:11 -0400

    Adding issuer address as param

--- test/Factory.js
+++ test/Factory.js
@@ -10,9 +10,12 @@ contract("Factory", async accounts => {
   });
 
   it("Should use factory to deploy new token", async () => {
-    Token = await Factory.deployNewToken("Demo Token", "DEMO", 1000000, {
-      from: accounts[0]
-    });
+    Token = await Factory.deployNewToken(
+      "Demo Token",
+      "DEMO",
+      1000000,
+      accounts[0]
+    );
     let TokenInstance = await TOKEN.at(Token.logs[0].args.tokenAddress);
     let balance = await TokenInstance.balanceOf.call(accounts[0]);
     assert.equal(balance.valueOf(), 1000000);
